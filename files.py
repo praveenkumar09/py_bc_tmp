6ffa4a0ed7c48c76ee479b6f68daeba1cd924562
praveenkumar09 <tpkprav@gmail.com>
2025-07-26 21:30:19 +0800

    Added example for with_file

--- files.py
+++ files.py
@@ -15,3 +15,10 @@ file_read_lines = open('demo.txt',mode='r')
 file_read_lines_content = file_read_lines.readlines()
 file_read_lines.close()
 print(file_read_lines_content)
+
+with open("demo.txt",mode='r') as file_with_ob:
+    file_with_ob_content_byLine = file_with_ob.readline()
+    while file_with_ob_content_byLine:
+        print(file_with_ob_content_byLine)
+        file_with_ob_content_byLine = file_with_ob.readline()
+print('Done!')        

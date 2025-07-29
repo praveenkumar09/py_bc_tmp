fd03a57c19888673713ca8099a8ff151e98d4d99
praveenkumar09 <tpkprav@gmail.com>
2025-07-29 09:42:23 +0800

    Added Printable base class to output dictionary

--- transaction.py
+++ transaction.py
@@ -1,6 +1,7 @@
 from collections import OrderedDict
+from printable import Printable
 
-class Transaction:
+class Transaction(Printable):
     
     def __init__(self,sender, recipient, amount):
         self.sender = sender
@@ -9,5 +10,3 @@ class Transaction:
         
     def to_ordered_dict(self):
         return OrderedDict([('sender',self.sender),('recipient',self.recipient),('amount',self.amount)])
-        
-    
